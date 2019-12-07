ab9ebfe0f4d099ebd16224848c9ec6d392024d2c
Sergey <sergey.morenets@yandex.ua>
2019-12-07 12:04:33 +0200

    Task #9.2. Add integration between book and statistics application

--- book/src/main/java/it/discovery/book/controller/BookController.java
+++ book/src/main/java/it/discovery/book/controller/BookController.java
@@ -5,6 +5,7 @@ import it.discovery.book.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -37,7 +38,20 @@ public class BookController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping("{id}")
+    public BookDTO findOne(@PathVariable int id) {
+        BookDTO bookDTO = new BookDTO(bookRepository.findBookById(id),
+                getHitCount(id));
+        saveHit(id);
+        return bookDTO;
+    }
+
     private int getHitCount(int id) {
         return restTemplate.getForObject("http://statistics/hit/" + id, Integer.class);
+    }
+
+    private void saveHit(int bookId) {
+        restTemplate.postForObject("http://statistics/hit/" + bookId,
+                null, Void.class);
     }
 }
--- statistics/src/main/java/it/discovery/statistics/controller/HitController.java
+++ statistics/src/main/java/it/discovery/statistics/controller/HitController.java
@@ -1,11 +1,11 @@
 package it.discovery.statistics.controller;
 
+import it.discovery.statistics.domain.Hit;
 import it.discovery.statistics.repository.HitRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("hits")
@@ -17,5 +17,13 @@ public class HitController {
     @GetMapping("{id}")
     public int getHitCount(@PathVariable int id) {
         return hitRepository.countByObjectId(id);
+    }
+
+    @PostMapping("{objectId}")
+    public void save(@PathVariable int objectId) {
+        Hit hit = new Hit();
+        hit.setObjectId(objectId);
+        hit.setViewed(LocalDateTime.now());
+        hitRepository.save(hit);
     }
 }
