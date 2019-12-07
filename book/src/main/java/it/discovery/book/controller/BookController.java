56f59fd02fd9976f5fca43ab46e34a09dfe025cf
Sergey <sergey.morenets@yandex.ua>
2019-12-07 11:46:57 +0200

    Task #9.1. Add Ribbon support for book service

--- book/src/main/java/it/discovery/book/controller/BookController.java
+++ book/src/main/java/it/discovery/book/controller/BookController.java
@@ -1,19 +1,43 @@
 package it.discovery.book.controller;
 
+import it.discovery.book.dto.BookDTO;
+import it.discovery.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("book")
+@RequiredArgsConstructor
 public class BookController {
 
     @Value("${library}")
     private String libraryName;
 
+    private final BookRepository bookRepository;
+
+    private final RestTemplate restTemplate;
+
     @GetMapping("library")
     public String getLibraryName() {
         return libraryName;
+    }
+
+    @GetMapping
+    public List<BookDTO> findAll() {
+        return bookRepository.getBooks()
+                .stream()
+                .map(book -> new BookDTO(book, getHitCount(book.getId())))
+                .collect(Collectors.toList());
+    }
+
+    private int getHitCount(int id) {
+        return restTemplate.getForObject("http://statistics/hit/" + id, Integer.class);
     }
 }
---
+++ book/src/main/java/it/discovery/book/dto/BookDTO.java
@@ -0,0 +1,36 @@
+package it.discovery.book.dto;
+
+import it.discovery.book.domain.Book;
+import it.discovery.book.domain.Person;
+import lombok.Getter;
+import lombok.Setter;
+
+import static java.util.Optional.ofNullable;
+
+@Getter
+@Setter
+public class BookDTO {
+    private int id;
+
+    private String name;
+
+    private int year;
+
+    private int pages;
+
+    private double price;
+
+    private int authorId;
+
+    private int hitCount;
+
+    public BookDTO(Book book, int hitCount) {
+        this.hitCount = hitCount;
+        id = book.getId();
+        name = book.getName();
+        year = book.getYear();
+        pages = book.getPages();
+        price = book.getPrice();
+        authorId = ofNullable(book.getAuthor()).map(Person::getId).orElse(null);
+    }
+}
